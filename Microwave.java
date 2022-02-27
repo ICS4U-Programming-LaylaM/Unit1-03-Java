8642341a8cdcb7242dbdd45f0d0192487b3acb47
Ubuntu <ubuntu@ip-172-31-88-254.ec2.internal>
2022-02-27 08:14:24 +0000

    Added comments

--- Microwave.java
+++ Microwave.java
@@ -48,11 +48,14 @@ final class Microwave {
         double time;
 
         while (true) {
+            // Get food item
             System.out.print("What food will you be "
                 + "eating (sub, pizza, soup)? ");
             String userFood = myObj.nextLine();
+            // Makes it not case sensitive
             userFood = userFood.toLowerCase();
 
+            // Check if userFood is any of the 3
             if ("sub".equals(userFood)) {
                 time = SIXTY;
                 break;
@@ -63,38 +66,50 @@ final class Microwave {
                 time = HUNDRED_FIVE;
                 break;
             } else {
+                // Error message if it's none of the 3
                 System.out.print("Please enter one of the three options.\n\n");
             }
         }
 
         while (true) {
+            // Get quantity
             System.out.print("\nHow many will you be eating (1 to 3)? ");
             final String quantity = myObj.nextLine();
             try {
+                // Check if quantity is an integer
                 final int quantityInt = Integer.parseInt(quantity);
 
+                // Check if quantity is between 1 and 3
                 if (quantityInt < 1 || quantityInt > THREE) {
                     System.out.print("Quantity must be between 1 and 3.\n\n");
                 } else {
                     if (quantityInt == 2) {
+                        // New time based on quantity
                         final double newTime = time * 1.5;
+                        // Convert into minutes
                         final double minutes = newTime / SIXTY;
+                        // Display how long it needs to microwave for
                         System.out.print("Mircrowave for " + minutes
                             + " minutes.\n");
                     } else if (quantityInt == THREE) {
+                        // New time based on quantity
                         final double newTime = time * 2;
+                        // Convert into minutes
                         final double minutes = newTime / SIXTY;
+                        // Display how long it needs to microwave for
                         System.out.print("Mircrowave for " + minutes
                             + " minutes.\n");
                     } else {
-                        final double newTime = time;
-                        final double minutes = newTime / SIXTY;
+                        // Convert into minutes
+                        final double minutes = time / SIXTY;
+                        // Display how long it needs to microwave for
                         System.out.print("Mircrowave for " + minutes
                             + " minutes.\n");
                     }
                     break;
                 }
             } catch (NumberFormatException ex) {
+                // Error message if quantity isn't an integer
                 System.out.print("Please enter a valid integer.\n\n");
             }
         }
